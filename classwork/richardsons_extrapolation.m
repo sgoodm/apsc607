39aaf37181a34d71e2d9126efb9d85ebd386a64d
userw <sgoodman@aiddata.org>
2017-10-15 20:19:04 -0400

    Update richardsons extrapolation

--- classwork/richardsons_extrapolation.m
+++ classwork/richardsons_extrapolation.m
@@ -1,88 +1,45 @@
 % class ?
 % richardson's extrapolation
 
-
-
 syms x
 sym_f = x*exp(x);
 
+f = matlabFunction(sym_f);
+sym_f1 = diff(sym_f);
 
-f = matlabFunction(sym_f)
-sym_f1 = diff(sym_f)
-
-f1 = matlabFunction(sym_f1)
-
-f = @(x) x*exp(x);
-x0 = 2;
+f1 = matlabFunction(sym_f1);
 
 % second order
-n1 = @(f, x0, h) (1/(2*h)) * (f(x0+h) - f(x0-h));
-
+n1 = @(f, x0, h) (1./(2.*h)) .* (f(x0+h) - f(x0-h));
 % fourth order
-n2 = @(f, x0, h) n1(f, x0, h/2) + (1/3)*(n1(f, x0, h/2) - n1(f, x0, h));
-
+n2 = @(f, x0, h) n1(f, x0, h./2) + (1./3).*(n1(f, x0, h./2) - n1(f, x0, h));
 % sixth order
-n3 = @(f, x0, h) n2(f, x0, h/2) + (1/15)*(n2(f, x0, h/2) - n1(f, x0, h));
+n3 = @(f, x0, h) n2(f, x0, h./2) + (1./15).*(n2(f, x0, h./2) - n1(f, x0, h));
 
-
-n1(f, x0, 0.05)
-n2(f, x0, 0.1)
-n3(f, x0, 0.2)
-
-
-
+h = 0.1;
 range = 0:h:2*pi;
 
-slope = [];
+o2 = n1(f, range, h);
+o4 = n2(f, range, h);
+o6 = n3(f, range, h);
 
-
-count = length(range);
-step_iter = 1:count; 
-m = zeros(count, count);
-
-
-for i=step_iter
-   
-    x0 = range(i);
-    
-    if x0 == min(range) || x0 == max(range)
-        a = -3/(2*h);
-        b = 4/(2*h);
-        c = -1/(2*h);
-        slope(i) = a * f(x0) + b * f(x0+h)  + c * f(x0+2*h);
-    else
-        a = -1/(2*h);
-        b = 0;
-        c = 1/(2*h);
-        slope(i) = a * f(x0 - h) + b * f(x0) + c * f(x0 +h);
-    end
-    
-    coef = [a b c];
-    
-    if i == 1
-        col = 1;
-    elseif i == count
-        col = count - (length(coef)-1);
-        coef = fliplr(coef);
-    else 
-        col = i - 1;
-    end
-    
-    m(i, col:col+(length(coef)-1)) = coef;
-
-           
-end
-
-true = f(range);
-
-out = m * true.';
-
+true_func = f(range);
+true_diff = f1(range);
 
 close all;
-figure()
-plot(range, true, 'r');
+plot(range, true_func, 'r');
 hold on;
-plot(range, val, 'b');
-plot(range, out, 'c--')
-legend('Function', 'Slope Calc', 'Slope Matrix', 'Location', 'best');
+plot(range, true_diff, 'g');
+plot(range, o2, 'b--');
+plot(range, o4, 'c--')
+plot(range, o6, 'm--')
+legend('True Func', 'True Diff', '2nd Order', '4th Order', '6th Order', 'Location', 'best');
+
+figure()
+plot(range, abs(true_diff-o2), 'b--');
+hold on
+plot(range, abs(true_diff-o4), 'c--')
+plot(range, abs(true_diff-o6), 'm--')
+legend('2nd Order Error', '4th Order Error', '6th Order Error', 'Location', 'best');
+set(gca, 'YScale', 'log')
 
