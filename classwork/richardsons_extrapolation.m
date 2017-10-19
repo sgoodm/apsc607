a18351861f357ca597b27ae724551b8c887600b6
userz <sgoodman@aiddata.org>
2017-10-19 17:03:47 -0400

    Fix equation

--- classwork/richardsons_extrapolation.m
+++ classwork/richardsons_extrapolation.m
@@ -1,6 +1,8 @@
 % class ?
 % richardson's extrapolation
 
+format long;
+
 syms x
 sym_f = x*exp(x);
 
@@ -14,7 +16,7 @@ n1 = @(f, x0, h) (1./(2.*h)) .* (f(x0+h) - f(x0-h));
 % fourth order
 n2 = @(f, x0, h) n1(f, x0, h./2) + (1./3).*(n1(f, x0, h./2) - n1(f, x0, h));
 % sixth order
-n3 = @(f, x0, h) n2(f, x0, h./2) + (1./15).*(n2(f, x0, h./2) - n1(f, x0, h));
+n3 = @(f, x0, h) n2(f, x0, h./2) + (1./15).*(n2(f, x0, h./2) - n2(f, x0, h));
 
 h = 0.1;
 range = 0:h:2*pi;
@@ -42,4 +44,7 @@ plot(range, abs(true_diff-o4), 'c--')
 plot(range, abs(true_diff-o6), 'm--')
 legend('2nd Order Error', '4th Order Error', '6th Order Error', 'Location', 'best');
 set(gca, 'YScale', 'log')
+
+
+
 
